ad0dc86fde9cba11cf19a6cc8ee95f77ce1e9824
aadshalshihry <aalshehri08@hotmail.com>
2015-11-12 18:04:36 -0500

    Prepere the project to start.

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Develop a recursive method to determine the number of 
+ *                  distinct ways in which a given amount of money in cents
+ *                  can be changed into quarters, dimes, nickels, and pennies
+ *
+ *        Version:  1.0
+ *        Created:  11/07/2015 21:51:45
+ *       Compiler:  gcc
+ *
+ *         Author:  Roman - Abdulrahman Alshehri, aalshehri@umassd.edu
+ *
+ * =============================================================================
+ */
+
+#include "coinWay.h"
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
